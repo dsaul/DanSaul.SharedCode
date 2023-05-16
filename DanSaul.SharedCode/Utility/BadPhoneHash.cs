d3a6de5f49326a36016ce409d315c4622fe665ee
Dan <dan@dsaul.ca>
2023-05-16 12:22:36 -0500

    refactor

---
+++ DanSaul.SharedCode/Utility/BadPhoneHash.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using DanSaul.SharedCode.Extensions;
+
+namespace DanSaul.SharedCode.Utility
+{
+    public static class BadPhoneHash
+    {
+        public static string CreateBadPhoneHash(string input, int length = 6)
+        {
+            // Use input string to calculate MD5 hash
+            using System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+
+
+            string md5str = sb.ToString();
+            md5str = md5str.ToLower();
+
+            Dictionary<string, string> map = new()
+            {
+                { @"a", @"2" },
+                { @"b", @"2" },
+                { @"c", @"2" },
+                { @"d", @"3" },
+                { @"e", @"3" },
+                { @"f", @"3" },
+                { @"g", @"4" },
+                { @"h", @"4" },
+                { @"i", @"4" },
+                { @"j", @"5" },
+                { @"k", @"5" },
+                { @"l", @"5" },
+                { @"m", @"6" },
+                { @"n", @"6" },
+                { @"o", @"6" },
+                { @"p", @"7" },
+                { @"q", @"7" },
+                { @"r", @"7" },
+                { @"s", @"7" },
+                { @"t", @"8" },
+                { @"u", @"8" },
+                { @"v", @"8" },
+                { @"w", @"9" },
+                { @"x", @"9" },
+                { @"y", @"9" },
+                { @"z", @"9" }
+            };
+
+            return md5str.MapReplace(map)[..length];
+        }
+    }
+}
---
+++ DanSaul.SharedCode/Utility/ISO8601Compare.cs
@@ -0,0 +1,14 @@
+﻿namespace DanSaul.SharedCode.Utility
+{
+    public static class ISO8601Compare
+    {
+        public static int Compare(string iso1, string iso2)
+        {
+            DateTime dt1 = DateTime.Parse(iso1, Culture.DevelopmentCulture);
+            DateTime dt2 = DateTime.Parse(iso2, Culture.DevelopmentCulture);
+
+            return dt1.CompareTo(dt2);
+        }
+
+    }
+}
