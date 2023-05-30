35f8b45096f45a773d58ae388584c897c193d3f0
Dan <dan@dsaul.ca>
2023-05-30 15:22:54 -0500

    fix resources

---
+++ DanSaul.SharedCode/Utility/DiscoverWANIP.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace DanSaul.SharedCode.Utility
+{
+    public static class DiscoverWANIP
+	{
+		public static async Task<IPAddress?> Run()
+		{
+			using HttpClient client = new HttpClient();
+			string str = await client.GetStringAsync("https://api.ipify.org");
+			if (!IPAddress.TryParse(str, out IPAddress? _parsed))
+				return null;
+			return _parsed;
+		}
+	}
+}
